69a4bfef2cbc23af195907d2fd7d5fe466dbcb12
Brandon9353 <brandonumana2000@gmail.com>
2024-07-29 20:49:43 -0600

    Update api_keys.py

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,12 +1,12 @@
 # OpenWeatherMap API Key
 <<<<<<< HEAD
-weather_api_key = "623021c77869cdb49a887dcf2438cd4d"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "9c46ddddad7e45f2be8e0dc012bcba50"
+geoapify_key = ""
 =======
 weather_api_key = "YOUR KEY HERE"
 
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
->>>>>>> 6df56c90bccabe4f7b1553c25c24391004d74cb8
+>>>>>>> 
