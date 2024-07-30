c0a58113fb52d4e312bd2970bf1a295bb9f89f48
Brandon9353 <brandonumana2000@gmail.com>
2024-07-29 20:47:48 -0600

    starting project

---
+++ Starter_Code/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "623021c77869cdb49a887dcf2438cd4d"
+
+# Geoapify API Key
+geoapify_key = "9c46ddddad7e45f2be8e0dc012bcba50"
---
+++ Starter_Code/api_keys.py
@@ -0,0 +1,12 @@
+# OpenWeatherMap API Key
+<<<<<<< HEAD
+weather_api_key = "623021c77869cdb49a887dcf2438cd4d"
+
+# Geoapify API Key
+geoapify_key = "9c46ddddad7e45f2be8e0dc012bcba50"
+=======
+weather_api_key = "YOUR KEY HERE"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
+>>>>>>> 6df56c90bccabe4f7b1553c25c24391004d74cb8
